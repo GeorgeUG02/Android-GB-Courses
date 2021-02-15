d24f455c801e621b15f9f42382f50e2a8b3f6ce7
George <georgukladnik@gmail.com>
2021-02-15 23:13:25 +0300

    lesson12

--- Lesson12/lesson12-homework/src/com/company/Main.java
+++ Lesson12/lesson12-homework/src/com/company/Main.java
@@ -7,12 +7,12 @@ import java.util.concurrent.Executors;
 public class Main {
     static final int SIZE = 10_000_000;
     static final int HALF = SIZE / 2;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
          arr1();
          arr2();
     }
 
-    private static void arr2() {
+    private static void arr2() throws InterruptedException {
         float[] arr = new float[SIZE];
         float[] a1 = new float[HALF];
         float[] a2 = new float[HALF];
@@ -20,21 +20,22 @@ public class Main {
         long a = System.currentTimeMillis();
         System.arraycopy(arr, 0, a1, 0, HALF);
         System.arraycopy(arr, HALF, a2, 0, HALF);
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        executorService.execute(()->{
+        Thread t1=new Thread(()->{
             for(int i=0;i<HALF;i++){
                 a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
-        });
-        executorService.execute(()->{
+        });t1.start();
+        Thread t2=new Thread(()->{
             for(int i=HALF;i<SIZE;i++){
                 a2[i-HALF] = (float)(a2[i-HALF] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
-        });
-        executorService.shutdown();
+        });t2.start();
+        t1.join();
         System.arraycopy(a1, 0, arr, 0, HALF);
+        t2.join();
         System.arraycopy(a2, 0, arr, HALF, HALF);
         System.out.println(System.currentTimeMillis() - a);
+        System.out.println(arr[9999999]);
     }
 
     private static void arr1() {
@@ -45,5 +46,6 @@ public class Main {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         System.out.println(System.currentTimeMillis() - a);
+        System.out.println(arr[9999999]);
     }
 }
